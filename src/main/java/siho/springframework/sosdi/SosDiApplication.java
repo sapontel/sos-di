c3ca703a558c0b8b507ce459dd6f859e4db6cca2
Saul Osbaldo Aponte Lopez <ingsoal@gmail.com>
2021-01-19 11:33:49 -0500

    Start project empty without libraries
    This project is to teach dependency injection

---
+++ src/main/java/siho/springframework/sosdi/SosDiApplication.java
@@ -0,0 +1,13 @@
+package siho.springframework.sosdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/siho/springframework/sosdi/SosDiApplicationTests.java
@@ -0,0 +1,13 @@
+package siho.springframework.sosdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

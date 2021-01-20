b64574c2e4d4174b8f7ba16f0d12ea82d1c8788c
Saul Osbaldo Aponte Lopez <ingsoal@gmail.com>
2021-01-19 20:42:56 -0500

    Classes were annotated with @Profile ({"", "default})

--- src/main/java/siho/springframework/sosdi/controllers/I18nSpanishGreetingService.java
+++ src/main/java/siho/springframework/sosdi/controllers/I18nSpanishGreetingService.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import siho.springframework.sosdi.services.GreetingService;
 
-@Profile("ES")
+@Profile({"ES","default"})
+//@Profile("ES")
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
     @Override
